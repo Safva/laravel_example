7908e180260a00fbd6aedce03d5615b0e8e64297
Safva <safva2000@gmail.com>
2021-10-21 20:04:42 +0530

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>document</title>
+</head>
+<body>
+    <div style="height: 600px; width: 600px;  margin: 25px 300px 75px 300px; border:10px">
+    <table cellpadding="10px"> 
+        <tr>
+            <th colspan="2"><h3 style="text-align:left;"> Sign Up </h3></th>
+        </tr>
+        <tr>
+            <td><input type="text" name="" id="" placeholder="First Lame" style="width:300px; height:25px"></td>
+            <td><input type="text" name="" id="" placeholder="Last Name" style="width:300px; height:25px"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="text" name="" id="" placeholder="Mobile Number" style="width:500px; height:25px "></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="email" name="" id="" placeholder="Email" style="width:500px; height:25px"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="password" name="" id="" placeholder="Password" style="width:500px; height:25px"></td>
+        </tr>
+
+        <tr>
+        <td><button type="submit" style="width:200px; height:40px; background-color: rgb(10, 136, 136);">Sign Up Now! </button></td> 
+        </tr>
+        
+        </table>
+    </div>
+</body>
+</html>
